245beb1a10daac8952c107355f422cf9ac486aba
teiwiet <omegalmao1.3@gmail.com>
2024-02-16 04:02:41 -0500

    update

---
+++ note.md
@@ -0,0 +1,15 @@
+# 1.Create a Cross compiler 
+https://wiki.osdev.org/GCC_Cross-Compiler
+## 1.1 Dependencies to downloads 
+- sudo apt install build-essential
+- sudo apt install bison
+- sudo apt install flex  
+- sudo apt install libgmp3-dev
+- sudo apt install libmpc-dev
+- sudo apt install libmpfr-dev
+- sudo apt install textinfo
+- sudo apt install libcloog-isl-dev 
+- sudo apt install libisl-dev 
+- https://ftp.gnu.org/gnu/binutils/binutils-2.35.tar.xz
+
+
---
+++ src/boot/boot.asm
@@ -0,0 +1,79 @@
+ORG 0x7c00
+BITS 16
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+_start:
+	jmp short start 
+	nop
+times 33 db 0
+start:
+	jmp 0:step2
+step2:
+	cli ; clear interrupt
+	mov ax,0x0
+	mov ds,ax
+	mov es,ax
+	mov ss,ax
+	mov sp,0x7c00
+	sti ; enable interrupt
+	jmp $
+
+
+.load_protected:
+	cli
+	lgdt [gdt_descriptor]
+	mov eax,cr0
+	or eax,0x1
+	mov cr0,eax
+	jmp CODE_SEG:load32
+;GDT
+gdt_start:
+
+gdt_null:
+	dd 0x0 
+	dd 0x0 
+
+gdt_code: ; cs should point to this address
+	dw 0xffff ; segment limit rist
+	dw 0      ; base first 0-15 bits
+	db 0      ; base next 16-31 bits
+	db 0x9a   ; access byte
+	db 11001111b ; high 4 bits flags and thw low 4 bit flags
+	db 0      ; base 24-31 bits
+; offset 0x10
+gdt_data: ; DS,ES,SS,FS,GS
+	dw 0xffff ; segment limit rist
+	dw 0      ; base first 0-15 bits
+	db 0      ; base next 16-31 bits
+	db 0x92   ; access byte
+	db 11001111b ; high 4 bits flags and thw low 4 bit flags
+	db 0      ; base 24-31 bits
+gdt_end:
+	
+gdt_descriptor:
+	dw gdt_end - gdt_start - 1
+	dd gdt_start 
+[BITS 32]
+
+load32:
+	mov ax,DATA_SEG
+	mov ds,ax
+	mov es,ax
+	mov fs,ax
+	mov gs,ax
+	mov ss,ax
+	mov ebp,0x00200000
+	mov esp,ebp
+
+	; Enable the A20 line
+	in al,0x92
+	or al,2 
+	out 0x92,al
+	jmp $
+error_message: db "Failed to load sector",0
+times 510 - ($-$$) db 0
+dw 0xAA55
+
+buffer:
+
+
